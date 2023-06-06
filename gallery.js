ddb8b003ecab540337cbf5a2cd8feb5776f7f411
fakhriyyatum muslimah <fakhriy1511@gmail.com>
2023-06-06 23:56:08 +0700

    commit 1

---
+++ gallery.js
@@ -0,0 +1,27 @@
+const image = [
+  {
+    nama: "kucing",
+    url: "https://drive.google.com/uc?export=view&id=1FfGNlTZAPolKvecGHwdTubSoGzvt4z6d",
+    kategori: "hewan",
+  },
+  {
+    nama: "anjing",
+    url: "https://drive.google.com/uc?export=view&id=1FfGNlTZAPolKvecGHwdTubSoGzvt4z6d",
+    kategori: "hewan",
+  },
+  {
+    nama: "singa",
+    url: "https://drive.google.com/uc?export=view&id=1FfGNlTZAPolKvecGHwdTubSoGzvt4z6d",
+    kategori: "hewan",
+  },
+  {
+    nama: "anjing cewe",
+    url: "https://drive.google.com/uc?export=view&id=1FfGNlTZAPolKvecGHwdTubSoGzvt4z6d",
+    kategori: "hewan",
+  },
+  {
+    nama: "kucing",
+    url: "https://drive.google.com/uc?export=view&id=1FfGNlTZAPolKvecGHwdTubSoGzvt4z6d",
+    kategori: "hewan",
+  },
+];
